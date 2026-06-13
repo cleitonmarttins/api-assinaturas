386d8794709769116a97a23b844fe4e0dfe374de
cleitonmarttins <cleitonmarttins@hotmail.com>
2026-06-12 21:43:23 -0300

    versão inicial

---
+++ src/main/java/br/com/cotiinformatica/api_assinaturas/entities/Assinatura.java
@@ -0,0 +1,41 @@
+package br.com.cotiinformatica.api_assinaturas.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "assinaturas")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Assinatura {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate dataAssintura;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal valor;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "plano_id", nullable = false)
+    private Plano plano;
+
+}
